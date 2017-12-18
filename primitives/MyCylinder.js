d39b5f46d53a2aa08f8341a9ca97d182b98c7163
Pedro Azevedo <up201306026@fe.up.pt>
2017-12-18 22:53:27 +0000

    Added Board relation with Logic

--- primitives/MyCylinder.js
+++ primitives/MyCylinder.js
@@ -1,68 +1,111 @@
 /**
- * Mycylinder
+ * MyCylinder
  * @constructor
+ * @args height, bottom radius, top radius, sections along height(stacks) and parts per section(slices), botFlag and topFlag
  */
- function MyCylinder(scene, slices, stacks) {
- 	CGFobject.call(this,scene);
-	
-	this.slices = slices;
-	this.stacks = stacks;
+function MyCylinder(scene, args) {
+    CGFobject.call(this, scene);
 
- 	this.initBuffers();
- };
+    this.height = args[0];
+    this.botRad = args[1];
+    this.topRad = args[2];
+    this.stacks = args[3];
+    this.slices = args[4];
+    this.topFlag = args[5];
+    this.botFlag = args[6];
 
- MyCylinder.prototype = Object.create(CGFobject.prototype);
- MyCylinder.prototype.constructor = MyCylinder;
+    //Testing
+    this.topFlag = 1;
+   this.botFlag = 1;
 
- MyCylinder.prototype.initBuffers = function() {
- 	/*
- 	* TODO:
- 	* Replace the following lines in order to build a cylinder with a **single mesh**.
- 	*
- 	* How can the vertices, indices and normals arrays be defined to
- 	* build a cylinder with varying number of slices and stacks?
- 	*/
+    this.initBuffers();
+};
 
- 	this.vertices = [];
- 	this.indices = [];
- 	this.normals = [];
- 	this.texCoords = [];
+MyCylinder.prototype = Object.create(CGFobject.prototype);
+MyCylinder.prototype.constructor = MyCylinder;
 
-	var ang = 2*Math.PI/this.slices;
-	
-	for(var j = 0; j < this.stacks+1; j++){
-		
-		var z = (j/this.stacks);
+MyCylinder.prototype.initBuffers = function() {
 
-		for(var i = 0; i < this.slices; i++)
-		{
-			this.vertices.push(Math.cos(i * ang),Math.sin(i * ang), z);
-			this.normals.push(Math.cos(i * ang),Math.sin(i * ang), 0);
-			this.texCoords.push(i/this.slices,j/this.stacks);
-		}
+    var stepAng = 2 * Math.PI / this.slices;
+    var currRadius = this.botRad;
+    var radiusInc = (this.topRad - this.botRad)/this.stacks;
 
-	}
+    this.vertices = new Array();
+    this.indices = new Array();1
+    this.normals = new Array();
+    this.texCoords = new Array();
 
+    var deltaS = 1/this.slices;
+    var deltaT = 1/this.stacks;
 
-	for(var j = 0; j < this.stacks; j++){
-		for(var i = 0; i < this.slices; i++)
-		{
-			this.indices.push(this.slices*j+i,this.slices*j+i+1,this.slices*(j+1)+i);
-			if (i != (this.slices - 1)) {
-				this.indices.push(this.slices*(j+1)+i+1,this.slices*(j+1)+i,this.slices*j+i+1);
+	var depth = this.height/this.stacks;
+
+ 	for (let i = 0; i <=this.stacks; i++){
+		for (let j = 0; j <= this.slices; j++){
+			this.vertices.push(currRadius * Math.cos(j*stepAng), currRadius * Math.sin(j*stepAng), i*depth);
+			this.normals.push(currRadius * Math.cos(j*stepAng), currRadius * Math.sin(j*stepAng), 0);
+			this.texCoords.push(j*deltaS, i*deltaT);
+
+			if (i < this.stacks) {
+				this.indices.push((i*this.slices)+j+i, (i*this.slices)+this.slices+j+1+i, i*(this.slices)+this.slices+j+i);
+				this.indices.push((i*this.slices)+j+i, (i*this.slices)+j+1+i, i*(this.slices)+this.slices+j+1+i);
 			}
-			else {
-				this.indices.push(this.slices*j,this.slices*j+i+1,this.slices*j+i);
-			}
-			
 		}
+        currRadius += radiusInc;
+ 	}
 
-	}
+  //------------------LIDS-----------------------------------------------------
+  this.topFlag = 1;
+  this.botFlag = 1;
 
 
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
+    if(this.topFlag){ //Z++
+  		this.vertices.push(0, 0, this.height);
+  		this.normals.push(0, 0, 1);
+  		this.texCoords.push(0.5, 0.5);
 
-MyCylinder.prototype.scaleTexCoords = function(ampS, ampT) {
-}
+  		let centerVertIdx = this.vertices.length/3-1;
+
+
+      for (let i = 0; i < this.slices; i++){
+    		this.vertices.push(this.topRad*Math.cos(i*stepAng), this.topRad*Math.sin(i*stepAng), this.height);
+    		this.normals.push(0, 0, 1);
+    		this.texCoords.push(Math.cos(i*stepAng)/2 + 0.5, 1- (Math.sin(i*stepAng)/2 + 0.5));
+
+    		this.indices.push(centerVertIdx + 0,  centerVertIdx+ i+1, centerVertIdx + i+2);
+	     }
+
+
+		this.vertices.push(this.topRad*1, 0, this.height);
+		this.normals.push(0, 0, 1);
+		this.texCoords.push(1, 0.5);
+
+  }
+
+    if(this.botFlag){ //Z--
+  		this.vertices.push(0, 0, 0);
+  		this.normals.push(0, 0, -1);
+  		this.texCoords.push(0.5, 0.5);
+
+  		let centerVertIdx = this.vertices.length/3 - 1;
+
+
+      for (let i = 0; i < this.slices; i++){
+      		this.vertices.push(this.botRad * Math.cos(i*stepAng), this.botRad * Math.sin(i*stepAng), 0);
+      		this.normals.push(0, 0, -1);
+      		this.texCoords.push(Math.cos(i*stepAng)/2 + 0.5, 1 - (Math.sin(i*stepAng)/2 + 0.5));
+
+          this.indices.push(centerVertIdx + 0,  centerVertIdx + i + 2, centerVertIdx + i + 1);
+      }
+
+  		this.vertices.push(this.botRad*1, 0, 0);
+  		this.normals.push(0, 0, -1);
+  		this.texCoords.push(1, 0.5);
+
+    }
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+};
+
+MyCylinder.prototype.updateTexCoords = function (sFactor, tFactor) {};
